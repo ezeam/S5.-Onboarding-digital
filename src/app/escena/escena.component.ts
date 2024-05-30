95fd16e4f7bb2ac0202bdde0dabee5957c729a11
Ester <ester.zea.maldonado@gmail.com>
2024-05-30 13:28:10 +0200

    WIP-lçogica y doom acabados, falta nivel 3, animaciones

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { iStep  } from '../Interfaces/iStep';
 import { CommonModule } from '@angular/common';
 
@@ -9,22 +9,39 @@ import { CommonModule } from '@angular/common';
   templateUrl: './escena.component.html',
   styleUrl: './escena.component.css'
 })
-export class EscenaComponent {  
+export class EscenaComponent implements OnInit{
+/*setCurrentStep(_t20: number) {
+throw new Error('Method not implemented.');
+}  */
   @Input() steps: iStep[] = [];
 
   currentStep: number = 0;
 
-  irAdelante(){
-   const totalPasos: number = 3;
-
-   if(this.currentStep <= totalPasos){
-    this.currentStep++;
-   }
+  ngOnInit(): void {
+    console.log("CurrentStep al principio:", this.currentStep);
+    //this.setCurrentStep();
   }
 
-  isAtras(){
-    if(this.currentStep > 0){
-      this,this.currentStep--;
+  setCurrentStep(index: number) {
+    this.currentStep = index;
+    console.log("Current step:", this.currentStep);
+  }
+
+  irAdelante(i:number) {
+    if (this.currentStep >= 0 && this.currentStep < this.steps.length -1) {      
+      this.currentStep++;
+      console.log("current step adelante",this.currentStep);
+    } else {
+      this.currentStep = this.steps.length - 1; // Si está en el primer paso, vuelve al último
+    }
+  }
+
+  irAtras(i: number) {
+    if (this.currentStep > 0 && this.currentStep < this.steps.length) {
+      this.currentStep--;
+      console.log("current step adelante",this.currentStep);
+    } else {
+      this.currentStep = 0; // Si está en el último paso, vuelve al primero
     }
   }
 }
